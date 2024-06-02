a39f5725bf2ffc9a4d467c7b70834a1e6c9779ab
avishek195 <avishekmistry420@gmail.com>
2024-06-02 20:47:32 +0530

    fix and DRY my code

--- script.js
+++ script.js
@@ -15,32 +15,31 @@ function changePicture(color) {
   image3.src = `img/undraw_conceptual_idea_${color}.svg`;
 }
 
-//Dark Mode Style
-function darkMode() {
-  nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
-  textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
-  toggleIcon.children[0].textContent = "Dark Mode";
-  toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
-  changePicture("dark");
-}
-//Light Mode Style
-function lightMode() {
-  nav.style.backgroundColor = "rgb(255 255 255/ 50%)";
-  textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
-  toggleIcon.children[0].textContent = "Light Mode";
-  toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
-  changePicture("light");
+//Toggle between Dark and Light
+
+function ToggleDarkLight(isDark) {
+  nav.style.backgroundColor = isDark
+    ? "rgb(0 0 0 / 50%)"
+    : "rgb(255 255 255/ 50%)";
+  textBox.style.backgroundColor = isDark
+    ? "rgb(255 255 255 / 50%)"
+    : "rgb(0 0 0 / 50%)";
+  toggleIcon.children[0].textContent = isDark ? "Dark Mode" : "Light Mode";
+  isDark
+    ? toggleIcon.children[1].classList.replace("fa-sun", "fa-moon")
+    : toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+  isDark ? changePicture("dark") : changePicture("light");
 }
 
 function switchTheme(e) {
   if (e.target.checked) {
     localStorage.setItem("theme", "dark");
     document.documentElement.setAttribute("data-theme", "dark");
-    darkMode();
+    ToggleDarkLight(true);
   } else {
     localStorage.setItem("theme", "light");
     document.documentElement.setAttribute("data-theme", "light");
-    lightMode();
+    ToggleDarkLight(false);
   }
 }
 
@@ -55,6 +54,6 @@ if (currentTheme) {
   document.documentElement.setAttribute("data-theme", currentTheme);
   if (currentTheme === "dark") {
     toggleSwitch.checked = true;
-    darkMode();
+    ToggleDarkLight(true);
   }
 }
